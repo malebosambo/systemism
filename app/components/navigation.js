f2962f188a1ede26a920ba9e640b9a24c77213ee
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-05-29 10:02:39 +0200

    Update navigation.js

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -3,17 +3,17 @@ import Link from 'next/link'
 export default function Navigation() {
   
   return (
-    <>
-      <Link href="/"><h6>Home</h6></Link>
-      <Link href="/invest"><h6>Invest</h6></Link>
-      <Link href="/funds"><h6>Funding</h6></Link>
-      <Link href="/shop"><h6>Shop</h6></Link>
-      <Link href="/listing"><h6>Listing</h6></Link>
+    <div className="Navigation">
+      <Link href="/">Home</Link>
+      <Link href="/invest">Invest</Link>
+      <Link href="/funds">Funding</Link>
+      <Link href="/shop">Shop</Link>
+      <Link href="/listing">Listing</Link>
       <Link href="/trading">Trading</Link>
-      <Link href="/business-development">Business Development</Link>
-      <Link href="/contacts"><h6>Contacts</h6></Link>
-      <Link href="/login"><h6>Login</h6></Link>
-      <Link href="/signup"><h6>Sign Up</h6></Link>
-    </>  
+      <Link href="/business-development">SMME Development</Link>
+      <Link href="/contacts">Contacts</Link>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Sign Up</Link>
+    </div>  
   );
 }
