f6d645c94bbf7c02653610b1868cd47c53d34dfe
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-05-30 10:01:32 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
+import { Balthazar } from "next/font/google"; 
 import Header from "./components/header";
 import Footer from "./components/footer";
 import "./globals.css";
+
+const balthazar = Balthazar({
+  subsets: ["latin"],
+  weight: '400'
+});
 
 export const metadata = {
   title: "SYSTEMISM",
@@ -9,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={balthazar.className}>
       <body>
         <div><Header /></div>
         <div>{children}</div>
