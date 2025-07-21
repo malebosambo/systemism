7e55d45b8803856e73760f43784a142f1408bba2
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-21 13:47:11 +0200

    Update and rename app/shop/page.js to app/(user)/market/page.js

---
+++ app/(user)/market/page.js
@@ -0,0 +1,12 @@
+export default function Market() {
+  
+  return (
+    <main>
+      <div><h1>Market</h1></div>
+      
+      <div className="Market_Filters"></div>
+      <div classsName="Market_Search"></div>
+      
+    </main>
+  )
+}
