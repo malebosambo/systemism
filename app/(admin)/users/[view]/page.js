a132434d42b870c1678f7069e09716f5d9db44a4
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-21 13:51:18 +0200

    Update and rename app/users/view/[id].js to app/(admin)/users/[view]/page.js

--- app/(admin)/users/[view]/page.js
+++ app/(admin)/users/[view]/page.js
@@ -4,8 +4,8 @@ export default function ViewUser() {
   
   return (
     <main>
-      <div><Image src="" alt="Profile Image" width="" height="" /></div>
-      <h1></h1>
+      <div><h1></h1></div>
+
     </main>
   )
 }
