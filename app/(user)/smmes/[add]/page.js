f08c6b5a1a1efc704a2f56dc8ccb9d7fcd238d49
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-21 13:07:13 +0200

    Update and rename app/enterprises/add-business/page.js to app/(user)/smmes/[add]/page.js

--- app/(user)/smmes/[add]/page.js
+++ app/(user)/smmes/[add]/page.js
@@ -1,24 +1,25 @@
-export default function Enterprises() {
+export default function AddSMME() {
   
   return (
     <div>
-      <div><h1>Add New Business</h1></div>
+      <div><h1>Add New SMME</h1></div>
       
       <div>
         <Form action={}>
           <input type="text" name="Business Name" placeholder="Business Name" onChange={} value={} required />
           <input type="text" name="Enterprise Number" placeholder="Enterprise Number" onChange={} value={} required />
+    
           <ul>
             <li><input type="radio" name="Private" value="Private" required />Private</li>
             <li><input type="radio" name="Corporation" value="Close Corporation" required />Close Corporation</li>
           </ul>
+    
           <input type="textarea" name="Physical Address" placeholder="Physical Address" onChange={} value={} required />
           <input type="text" name="Directors" placeholder="Director Full Name" onChange={} value={} required />
-          <button type="submit"></button>
+    
+          <button type="submit">Add SMME</button>
         </Form>
       </div>
-      
-      
     </div>
   )
 }
