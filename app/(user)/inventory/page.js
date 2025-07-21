a57099fda13d581f74ef260c21e70219fb703370
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-21 13:15:52 +0200

    Update and rename app/inventory/page.js to app/(user)/inventory/page.js

--- app/(user)/inventory/page.js
+++ app/(user)/inventory/page.js
@@ -1,14 +1,10 @@
-export default function Vendor() {
+export default function Inventory() {
   
   return (
     <main>
       <div><h1>Vendor Inventory Management</h1></div>
       
       <div className="Marketplace_Filters">
-        
-      </div>
-      
-      <div>
         
       </div>
     </main>
