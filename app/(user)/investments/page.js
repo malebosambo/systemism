48fddfcf752ab0ffb1ce6dd8221fc52c8fc041c2
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-21 13:22:00 +0200

    Update and rename app/investments/page.js to app/(user)/investments/page.js

--- app/(user)/investments/page.js
+++ app/(user)/investments/page.js
@@ -3,14 +3,10 @@ import Link from "next/link";
 export default function Investments() {
   
   return (
-    <div>
+    <main>
       <div><h1>My Investments</h1></div>
       
       <div><Link href="/investments/invest">Invest</Link></div>
-      
-      <div>
-        
-      </div>
-    </div>
+    </main>
   )
 }
