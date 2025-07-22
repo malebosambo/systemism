a7d61b4eb2c3d2ee7d41792d7675157de1dd48ea
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-22 15:05:11 +0200

    Update page.js

--- app/(user)/funds/request/page.js
+++ app/(user)/funds/request/page.js
@@ -1,11 +1,8 @@
-import Form from "next/form";
-
 export default function RequestFunds() {
   
   return (
     <main>
       <div><h1>Resquest Business Funding</h1></div>
-      
     </main>
   )
 }
