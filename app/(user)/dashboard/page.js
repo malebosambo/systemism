4b49aa3d1a750971c17ad95d9f0975082da55a37
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-22 14:57:35 +0200

    Update page.js

--- app/(user)/dashboard/page.js
+++ app/(user)/dashboard/page.js
@@ -3,60 +3,48 @@ import Link from 'next/link';
 export default function Dashboard() {
   
   return (
-    <div>
+    <div className="Dashboard">
       <div className="Account_Overview">
         <div><h1>Hello</h1></div>
-        <div><h2>Account balance: </h2></div>
+        <div><h2>Account balance: 0</h2></div>
         <div><Link href="/account/deposit">Deposit</Link></div>
         <div><Link href="/account/withdraw">Withdraw</Link></div>
       </div>
       
       <div className="Investments_Overview">
-        <div>
-          <h4>Investments : </h4>
-        </div>
-        <div></div>
-        <Link href="/investments/">View</Link>
+        <div><h4>Investments: Active</h4></div>
+        <div><p>Nothing to display.</p></div>
+        <div><Link href="/investments">View</Link></div>
       </div>
       
       <div className="Funds_Overview">
-        <div>
-          <h4>Funds: Active</h4>
-        </div>
-        <div></div>
-        <Link href="/funds/active">View</Link>
+        <div><h4>Funds: Active</h4></div>
+        <div><p>Nothing to display.</p></div>
+        <div><Link href="/funds/active">View</Link></div>
       </div>
       
-      <div className="Shopping_Overview">
-        <div>
-          <h4>Shopping : Invoices</h4>
-        </div>
-        <div></div>
-        <Link href="/shop/my-invoices">View</Link>
+      <div className="Market_Overview">
+        <div><h4>Market: Invoices</h4></div>
+        <div><p>No invoices available.</p></div>
+        <div><Link href="/market/invoices">View</Link></div>
       </div>
       
       <div className="Inventory_Overview">
-        <div>
-          <h4>Inventory : Levels</h4>
-        </div>
-        <div></div>
-        <Link href="/marketplace/inventory">View</Link>
+        <div><h4>Inventory: Levels</h4></div>
+        <div><p>No inventory available.</p></div>
+        <div><Link href="/marketplace/inventory">View</Link></div>
       </div>
       
       <div className="Programs_Overview">
-        <div>
-          <h4>Programs : Enrolled</h4>
-        </div>
-        <div></div>
-        <Link href="/smme-hub/programs/enrolled">View</Link>
+        <div><h4>Programs: Enrolled</h4></div>
+        <div><p>No programs enrolled.</p></div>
+        <div><Link href="/programs/enrolled">View</Link></div>
       </div>
       
       <div className="Listings_Overview">
-        <div>
-          <h4>Directory : Listings</h4>
-        </div>
-        <div></div>
-        <Link href="/listing/my-listings"></Link>
+        <div><h4>Listings: Approved</h4></div>
+        <div><p>No listings available.</p></div>
+        <div><Link href="/listings/approved"></Link></div>
       </div>
     </div>
   );
