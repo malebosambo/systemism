4b50ef1317cbd9c24a1255aa488c7e8d8acc4528
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-07-22 15:31:17 +0200

    Update page.js

--- app/(user)/smmes/add/page.js
+++ app/(user)/smmes/add/page.js
@@ -8,16 +8,16 @@ export default function AddSMME() {
       
       <div>
         <Form action="">
-          <input type="text" name="Business Name" placeholder="Business Name" onChange={} value={} required />
-          <input type="text" name="Enterprise Number" placeholder="Enterprise Number" onChange={} value={} required />
+          <input type="text" name="Business Name" placeholder="Business Name" onChange="" value="" required />
+          <input type="text" name="Enterprise Number" placeholder="Enterprise Number" onChange="" value="" required />
     
           <ul>
             <li><input type="radio" name="Private" value="Private" required />Private</li>
             <li><input type="radio" name="Corporation" value="Close Corporation" required />Close Corporation</li>
           </ul>
     
-          <input type="textarea" name="Physical Address" placeholder="Physical Address" onChange={} value={} required />
-          <input type="text" name="Directors" placeholder="Director Full Name" onChange={} value={} required />
+          <input type="textarea" name="Physical Address" placeholder="Physical Address" onChange="" value="" required />
+          <input type="text" name="Directors" placeholder="Director Full Name" onChange="" value="" required />
     
           <button type="submit">Add SMME</button>
         </Form>
