3679aebdd509f410892fc5efbb55647972be6df8
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-10-21 12:45:49 +0200

    Implement signUp function for user registration

---
+++ app/lib/auth.js
@@ -0,0 +1,20 @@
+import { collection, setDoc, doc } from "firebase/firestore";
+import { db } from "/../../firebase.js";
+
+export async function signIn() {
+
+}
+
+export async function signUp(data) {
+  try {
+    await setDoc(doc(db, "users", 2), data);
+    console.log("Successful write to db.");
+  } catch {
+    console.log("Error writing to db.");
+  }
+
+}
+
+export async function signOut() {
+
+}
