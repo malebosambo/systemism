6e4e1e6da8581f9e5778645d9bc88d285cb7b446
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-10-23 13:39:10 +0200

    Update page.js

--- app/(root)/signup/page.js
+++ app/(root)/signup/page.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import { userSignUp } from "/../../lib/actions";
 
 
 export default function Signup() {
@@ -20,13 +21,19 @@ export default function Signup() {
       <div><h1>Sign Up</h1></div>
       
       <div>
-        <form action="">
+        <form action={userSignUp}>
           <input type="text" name="firstName" placeholder="First Name" onChange={(e) => setUser.firstName(e.target.value)} value={user.firstName} required />
+
           <input type="text" name="surname" placeholder="Surname" onChange={(e) => setUser.surname(e.target.value)} value={user.surname} required />
+
           <input type="email" name="email" placeholder="Email" onChange={(e) => setUser.email(e.target.value)} value={user.email} required />
+
           <input type="telephone" name="cellphone" placeholder="Cellphone" onChange={(e) => setUser.cellphone(e.target.value)} value={user.cellphone} required />
+
           <input type="password" name="password" placeholder="Password" onChange={(e) => setUser.password(e.target.value)} value={user.password} required />
-          <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e) => setUser.confirmPassword(e.target.value)} value={user.confirmPassword} />
+
+          <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e) => setUser.confirmPassword(e.target.value)} value={user.confirmPassword} required />
+
           <button type="submit" className="Button">Submit</button>
         </form>
       </div>
