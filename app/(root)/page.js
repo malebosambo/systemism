d007d17e0bb1634f66c290c0d9c9efc2b30669af
malebosambo <malebo.sambo@outlook.com>
2026-07-15 14:40:06 +0200

    update UI

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         <div><p>Join the all-in-one platform that provides financial independence, wealth generation and economic participation.</p></div>
         
         <div className="Home_Buttons">
-          <div className="Button"><Link href="/login">Log In</Link></div>
+          <div className="Button"><Link href="/login"><p>Log In</p></Link></div>
           <div className="Button"><Link href="/signup">Create Account</Link></div>
         </div>
       </div>
