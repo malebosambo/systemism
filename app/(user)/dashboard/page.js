f5e1dd45d4afc63f6ce04b72541b18d87c6fa368
malebosambo <malebo.sambo@outlook.com>
2026-09-01 11:04:45 +0200

    update page.js

--- app/(user)/dashboard/page.js
+++ app/(user)/dashboard/page.js
@@ -37,6 +37,7 @@ export default function Dashboard() {
           displayName: currentUser.displayName,
           photoURL: currentUser.photoURL,
         });
+        console.log(currentUser);
       } else {
         setUser(null);
       }
@@ -55,7 +56,7 @@ export default function Dashboard() {
     <div className="Dashboard">
       
       <div className="Account_Overview">
-        <div><h1>Hello, {user.email}</h1></div>
+        <div><h1>Hello, </h1></div>
         <div><h2>Account balance: 0</h2></div>
         <div><Link href="/account/deposit">Deposit</Link></div>
         <div><Link href="/account/withdraw">Withdraw</Link></div>
