056354a5c3223b670e8166ac8cf6a3e06ef3a63f
malebosambo <malebo.sambo@outlook.com>
2026-09-01 11:46:08 +0200

    update layout.js

--- app/(user)/layout.js
+++ app/(user)/layout.js
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/next"
 import UserHeader from "../components/userHeader";
+import "../globals.css";
 
 export default function UserLayout({ children }) {
   /*
